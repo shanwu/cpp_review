74caf43280f97501407e3e7ef6ca57077a2398dd
shanwu <sungzheguang@hotmail.com>
2018-01-22 23:13:14 +0800

    change: add 'reverse a string' practice

---
+++ leetcode/reverse_a_string.cpp
@@ -0,0 +1,22 @@
+#include <algorithm> // that's the library which make you able to apply reverse() function
+#include <iostream>
+
+class Solution 
+{
+public:
+    static std::string reverseString(std::string s)
+    {
+        std::reverse(s.begin(), s.end());
+        return s;
+    }
+};
+
+int main(int argc, char** argv) {
+    if(argc != 2)
+    {
+        std::cout << "Please enter string value" << std::endl;
+    }
+    
+    std::cout<< "origin: "<< argv[1] <<" --> "<<Solution::reverseString(argv[1])<< std::endl;
+    return 0;
+}
