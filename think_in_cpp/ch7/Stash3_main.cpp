2ea5ef9f008e54bbdc482f8d6ecc55fa0a9aa080
shanwu <sungzheguang@hotmail.com>
2018-07-02 21:31:40 +0800

    change: code formating

--- think_in_cpp/ch7/Stash3_main.cpp
+++ think_in_cpp/ch7/Stash3_main.cpp
@@ -3,30 +3,31 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main()
+{
     Stash intStash(sizeof(int));
-    
-    for(int i = 0; i < 100; i++) {
+
+    for (int i = 0; i < 100; i++) {
         intStash.add(&i);
     }
 
-    for(int j =0; j < intStash.count(); j++) {
-        std::cout << "intStash.fetch("<< j <<") = "
-             << *(int*)intStash.fetch(j)
-             << std::endl; 
+    for (int j = 0; j < intStash.count(); j++) {
+        std::cout << "intStash.fetch(" << j << ") = "
+                  << *(int*)intStash.fetch(j)
+                  << std::endl;
     }
 
     const int bufsize = 80;
     Stash stringStash(sizeof(char) * bufsize, 100);
     std::ifstream in("Stash3.h");
     std::string line;
-    while(getline(in, line)) {
+    while (getline(in, line)) {
         stringStash.add((char*)line.c_str());
     }
 
     int k = 0;
     char* cp;
-    while((cp = (char*)stringStash.fetch(k++))!=0) {
-        std::cout<< "stringStash.fetch("<< k <<") = " << cp << std::endl;
+    while ((cp = (char*)stringStash.fetch(k++)) != 0) {
+        std::cout << "stringStash.fetch(" << k << ") = " << cp << std::endl;
     }
 }
